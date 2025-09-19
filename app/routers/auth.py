419ed8f7d3ebcbbbe19e6ceae136b8c2fe55d337
xifOO <wedyi28111@gmail.com>
2025-09-19 21:44:51 +0300

    refactor: remove oauth2 scheme and get_current_user dependency

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,31 +1,22 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Request, Response
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, Request, Response
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.auth.authorization import create_access_token, create_refresh_token
 from app.auth.cookie import set_refresh_cookie
+from app.permissions import requires_authenticated
 from app.schemas.auth import AccessTokenResponse
-from app.schemas.user import UserBase, UserCreate, UserResponse, UserSchema
+from app.schemas.user import UserBase, UserCreate, UserResponse
 from app.services.users import UserService
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-
-
-def get_current_user(request: Request) -> UserSchema:
-    user = request.user
-    if not user.is_authenticated:
-        raise HTTPException(status_code=401, detail="Not authenticated")
-    return user.user
 
 
 @router.get("/me", response_model=UserResponse)
-async def get_user_profile(
-    user: Annotated[UserSchema, Depends(get_current_user)],
-    token: Annotated[str, Depends(oauth2_scheme)],
-):  # token for swagger :)
-    user_profile = await UserService().get_user_profile(user)
+@requires_authenticated()
+async def get_user_profile(request: Request):
+    user_profile = await UserService().get_user_profile(request.user.user)
     return user_profile
 
 
