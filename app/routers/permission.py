240d1b695a597c4795710a6b83ab500429f2f3ae
xifOO <wedyi28111@gmail.com>
2025-09-19 21:44:59 +0300

    feat: add permission checks to permission endpoints

--- app/routers/permission.py
+++ app/routers/permission.py
@@ -1,7 +1,8 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
+from app.permissions import requires_permission, requires_role
 from app.schemas.permission import PermissionCreate, PermissionResponse
 from app.schemas.role_permission import RoleToPermissionBase, RoleToPermissionCreate
 from app.services.permission import PermissionService
@@ -17,12 +18,16 @@ async def get_permissions():
 
 
 @router.post("/create", response_model=PermissionResponse)
-async def create_permission(permission_data: PermissionCreate):
+@requires_permission("permission", "create")
+async def create_permission(request: Request, permission_data: PermissionCreate):
     permission = await PermissionService().create(permission_data)
     return permission
 
 
 @router.post("/assign", response_model=RoleToPermissionBase)
-async def assign_role_to_permission(role_to_permission_data: RoleToPermissionCreate):
+@requires_role("admin")
+async def assign_role_to_permission(
+    request: Request, role_to_permission_data: RoleToPermissionCreate
+):
     role_to_permission = await RoleToPermissionService().create(role_to_permission_data)
     return role_to_permission
