99398516d98852a605f6990e7dd673deecca20fe
xifOO <wedyi28111@gmail.com>
2025-09-19 21:44:19 +0300

    feat: add http bearer token support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import HTTPBearer
 from starlette.middleware.authentication import AuthenticationMiddleware
 
 from app.middleware.auth_middleware import JWTAuthMiddleware
@@ -6,7 +7,9 @@ from app.routers.auth import router as auth_router
 from app.routers.permission import router as permission_router
 from app.routers.role import router as role_router
 
-app = FastAPI(title="API Async chat")
+app = FastAPI(
+    title="API Async chat", dependencies=[Depends(HTTPBearer(auto_error=False))]
+)
 
 app.include_router(auth_router)
 app.include_router(role_router)
