d1378b07c440aebf3da1488360bbb16c20588047
xifOO <wedyi28111@gmail.com>
2025-09-19 21:45:32 +0300

    feat: add method to find multiple roles with permissions

--- app/services/role.py
+++ app/services/role.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from app.db.db import db
 from app.models.models import Role
 from app.repositories.role_repository import RoleRepository
 from app.services._service import BaseService
@@ -5,4 +8,10 @@ from app.services._service import BaseService
 
 class RoleService(BaseService):
     def __init__(self) -> None:
-        super().__init__(RoleRepository(Role))
+        self.repository = RoleRepository(Role)
+
+    async def find_multiple_with_permissions(self, role_names: List[str]):
+        async with db.get_db_session() as session:
+            return await self.repository.find_multiple_with_permissions(
+                session, role_names
+            )
