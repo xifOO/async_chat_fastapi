1459dcb71ac2c5c024b159fddc0d478bc60fed8a
xifOO <wedyi28111@gmail.com>
2025-09-22 19:34:41 +0300

    test: add auth fixture for testing

--- app/tests/api/base.py
+++ app/tests/api/base.py
@@ -1,4 +1,8 @@
 from httpx import ASGITransport, AsyncClient
+from starlette.authentication import AuthCredentials
+from app.schemas.user import UserSchema
+from app.middleware.auth_middleware import _AuthenticatedUser, JWTAuthMiddleware
+import pytest
 import pytest_asyncio
 
 
@@ -12,3 +16,23 @@ async def async_client(test_db, monkeypatch):
     async with AsyncClient(transport=ASGITransport(app=app), 
                          base_url="http://test") as client:
         yield client
+
+
+@pytest.fixture
+def auth_patch(monkeypatch):
+    def _patch_user(roles):  
+        mock_user = UserSchema(
+            id=666,
+            username="mock_user",
+            email="mock@example.com",
+            roles=roles
+        )
+
+        async def mock_authenticate(self, conn):
+            credentials = AuthCredentials(["authenticated"] + roles)
+            return credentials, _AuthenticatedUser(mock_user)
+
+        monkeypatch.setattr(JWTAuthMiddleware, "authenticate", mock_authenticate)
+        return mock_user
+
+    return _patch_user
