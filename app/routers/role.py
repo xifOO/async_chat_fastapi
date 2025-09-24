c0eb59ff6caffb79a122269abb006815ffa77dfc
xifOO <wedyi28111@gmail.com>
2025-09-25 00:58:48 +0300

    refactor: replace permission decorators with requires_check

--- app/routers/role.py
+++ app/routers/role.py
@@ -2,7 +2,7 @@ from typing import List
 
 from fastapi import APIRouter, Request
 
-from app.permissions import requires_permission, requires_role
+from app.permissions import check_permission, check_role, requires_check
 from app.schemas.role import RoleCreate, RoleResponse
 from app.schemas.user_role import UserToRoleBase, UserToRoleCreate
 from app.services.role import RoleService
@@ -18,14 +18,14 @@ async def get_roles():
 
 
 @router.post("/create", response_model=RoleResponse)
-@requires_permission("role", "create")
+@requires_check(check_permission("role", "create"))
 async def create_role(request: Request, role_data: RoleCreate):
     role = await RoleService().create(role_data)
     return role
 
 
 @router.post("/assign", response_model=UserToRoleBase)
-@requires_role("admin")
+@requires_check(check_role("admin"))
 async def assign_role_to_user(request: Request, user_to_role_data: UserToRoleCreate):
     user_to_role = await UserToRoleService().create(user_to_role_data)
     return user_to_role
