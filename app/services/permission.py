199636805cab276458b8441aaafc7d058bde4ea6
xifOO <wedyi28111@gmail.com>
2025-10-05 02:11:40 +0300

    refactor: replace direct db session with factory

--- app/services/permission.py
+++ app/services/permission.py
@@ -9,7 +9,8 @@ class PermissionService(BaseService):
     def __init__(self) -> None:
         self.repository = PermissionRepository(Permission)
         self.response_schema = PermissionResponse
+        self.db_session_factory = postgres_db
 
     async def assing_to_role(self, permission_id: int, role_id: int) -> None:
-        async with postgres_db.get_db_session() as session:
+        async with self.db_session_factory() as session:
             await self.repository.assign_to_role(session, permission_id, role_id)
