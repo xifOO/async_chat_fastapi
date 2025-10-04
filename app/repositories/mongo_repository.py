ded155d3f99f2a5c282d01d871025453b7c3430f
xifOO <wedyi28111@gmail.com>
2025-10-05 02:11:19 +0300

    refactor: use MongoSession for database operations

--- app/repositories/mongo_repository.py
+++ app/repositories/mongo_repository.py
@@ -1,9 +1,9 @@
-from typing import Any, Dict, Optional, Type
+from typing import Type
 
 from bson import ObjectId
-from motor.core import AgnosticClientSession
 from motor.motor_asyncio import AsyncIOMotorCollection, AsyncIOMotorDatabase
 
+from app.db.mongo import MongoSession
 from app.repositories._repository import (
     AbstractRepository,
     CreateSchemaType,
@@ -22,40 +22,50 @@ class MongoDBRepository(
     def get_collection(self, db: AsyncIOMotorDatabase) -> AsyncIOMotorCollection:
         return db[self.collection_name]
 
+    def _transform_objectid_fields(self, filters: dict) -> dict:
+        filters = filters.copy()
+        for field in ["id", "conversationId", "authorId"]:
+            if field in filters:
+                _id = filters.pop(field)
+                if ObjectId.is_valid(_id):
+                    filters["_id" if field == "id" else field] = ObjectId(_id)
+        return filters
+
     async def create(
         self,
+        session: MongoSession,
         data: CreateSchemaType,
-        db: AsyncIOMotorDatabase,
-        session: AgnosticClientSession,
-    ):
-        collection = self.get_collection(db)
+    ) -> ModelType:
+        collection = self.get_collection(session.db)
 
         doc = data.model_dump(exclude_unset=True)
 
-        result = await collection.insert_one(doc, session=session)
+        result = await collection.insert_one(doc, session=session.session)
 
         created_doc = await collection.find_one(
-            {"_id": result.inserted_id}, session=session
+            {"_id": result.inserted_id}, session=session.session
         )
         return created_doc
 
-    async def find_one(
-        self, db: AsyncIOMotorDatabase, session: AgnosticClientSession, **filters
-    ):
-        collection = self.get_collection(db)
-        return await collection.find_one(filters, session=session)
+    async def find_one(self, session: MongoSession, **filters):
+        collection = self.get_collection(session.db)
+
+        filters = self._transform_objectid_fields(filters)
+
+        return await collection.find_one(filters, session=session.session)
 
     async def find_all(
         self,
-        db: AsyncIOMotorDatabase,
-        session: AgnosticClientSession,
+        session: MongoSession,
         skip: int = 0,
         limit: int = 100,
         **filters,
     ):
-        collection = self.get_collection(db)
+        collection = self.get_collection(session.db)
 
-        cursor = collection.find(filters, session=session)
+        filters = self._transform_objectid_fields(filters)
+
+        cursor = collection.find(filters, session=session.session)
 
         cursor = cursor.skip(skip).limit(limit)
 
@@ -63,42 +73,47 @@ class MongoDBRepository(
 
     async def update(
         self,
+        session: MongoSession,
         data: UpdateSchemaType,
-        db: AsyncIOMotorDatabase,
-        id: str | ObjectId,
-        session: AgnosticClientSession,
+        obj_id: str | ObjectId,
     ):
-        collection = self.get_collection(db)
+        collection = self.get_collection(session.db)
 
-        if isinstance(id, str):
-            id = ObjectId(id)
+        if isinstance(obj_id, str):
+            obj_id = ObjectId(obj_id)
 
         update_data = data.model_dump(exclude_unset=True)
 
         result = await collection.find_one_and_update(
-            {"_id": id}, {"$set": update_data}, return_document=True, session=session
+            {"_id": obj_id},
+            {"$set": update_data},
+            return_document=True,
+            session=session.session,
         )
         return result
 
     async def delete(
         self,
-        db: AsyncIOMotorDatabase,
-        id: str | ObjectId,
-        session: Optional[AgnosticClientSession] = None,
+        session: MongoSession,
+        obj_id: str | ObjectId,
     ) -> None:
-        collection = self.get_collection(db)
+        collection = self.get_collection(session.db)
 
-        if isinstance(id, str):
-            id = ObjectId(id)
+        if isinstance(obj_id, str):
+            obj_id = ObjectId(obj_id)
 
-        await collection.delete_one({"_id": id}, session=session)
+        await collection.delete_one({"_id": obj_id}, session=session.session)
 
     async def exists(
         self,
-        db: AsyncIOMotorDatabase,
-        filters: Dict[str, Any],
-        session: Optional[AgnosticClientSession] = None,
+        session: MongoSession,
+        **filters,
     ) -> bool:
-        collection = self.get_collection(db)
-        count = await collection.count_documents(filters, limit=1, session=session)
+        collection = self.get_collection(session.db)
+
+        filters = self._transform_objectid_fields(filters)
+
+        count = await collection.count_documents(
+            filters, limit=1, session=session.session
+        )
         return count > 0
