370d2853163558e824dacf79fab13d50437fc9f3
xifOO <wedyi28111@gmail.com>
2025-10-13 17:20:39 +0300

    feat: add AWS error exception classes

--- app/exceptions.py
+++ app/exceptions.py
@@ -6,3 +6,12 @@ from fastapi import HTTPException
 class RecordAlreadyExists(HTTPException):
     def __init__(self, status_code: int, detail: Any = None) -> None:
         super().__init__(status_code, detail)
+
+
+class AWSError(Exception): ...
+
+
+class AWSUploadError(AWSError): ...
+
+
+class AWSDownloadError(AWSError): ...
