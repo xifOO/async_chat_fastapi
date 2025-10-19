2a4c299e837676b74d98af1e42552f50936a79d9
xifOO <wedyi28111@gmail.com>
2025-10-19 21:09:34 +0300

    feat: refactor consumer to use ConsumerChannel and add batch consume

--- app/kafka/consumers.py
+++ app/kafka/consumers.py
@@ -1,13 +1,13 @@
-from typing import Iterable
+from typing import Iterable, List
 
-from app.types.channel import ConsumerChannelT
+from app.kafka.channel import ConsumerChannel
 from app.types.message import Message
 from app.types.transport import ConsumerT
 
 
 class Consumer(ConsumerT):
-    def __init__(self, channel: ConsumerChannelT) -> None:
-        self._channel = channel
+    def __init__(self, **kwargs) -> None:
+        self._channel = ConsumerChannel(**kwargs)
         self._closed = False
         self._subscribed = False
 
@@ -20,8 +20,17 @@ class Consumer(ConsumerT):
 
         return await self._channel.consume()
 
-    async def subscribe(self, topics: Iterable[str]) -> None:
-        await self._channel.subscribe(topics=list(topics))
+    async def consume_batch(self, max_records: int, timeout: int) -> List[Message]:
+        if self._closed:
+            raise RuntimeError("Consumer is closed")
+
+        if not self._subscribed:
+            raise RuntimeError("Consumer not subscribed")
+
+        return await self._channel.consume_batch(max_records, timeout)
+
+    def subscribe(self, topics: Iterable[str]) -> None:
+        self._channel.subscribe(topics=list(topics))
         self._subscribed = True
 
     async def commit(self) -> None:
@@ -30,5 +39,7 @@ class Consumer(ConsumerT):
     async def close(self) -> None:
         if self._closed:
             return
+        
         self._closed = True
         await self.commit()
+        await self._channel.stop()
