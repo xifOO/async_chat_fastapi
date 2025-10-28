3f19ab14cc2a3638f1644d30a7ddbdedb28b1946
xifOO <wedyi28111@gmail.com>
2025-10-29 01:02:33 +0300

    refactor: update message schema validation

--- app/utils.py
+++ app/utils.py
@@ -1,27 +1,29 @@
 from typing import List
 
 from app.cache import RedisManager
-from app.schemas.message import MessageResponse
+from app.schemas.message import CacheMessage, DBMessage, MessageType
 from app.services.message import MessageService
 
 
-async def load_messages(service: MessageService, conv_id: str) -> List[dict]:
+async def load_messages(service: MessageService, conv_id: str) -> List[MessageType]:
     redis = RedisManager()
     await redis.connect()
 
-    messages = []
+    messages: List[MessageType] = []
 
-    r_records = await redis.get_messages(
+    cached = await redis.get_messages(
         f"chat:{conv_id}:messages", batch_size=100
     )  # later from settings.batch_size
-    if r_records:
-        r_messages = [MessageResponse.model_validate(record) for record in r_records]
-        messages.extend(r_messages)
+    if cached:
+        messages.extend(CacheMessage.model_validate(r) for r in cached)
 
     remaining = 100 - len(messages)  # later from settings.batch_size
     if remaining > 0:
-        db_messages = await service.find_all(conversationId=conv_id, limit=remaining)
-        messages.extend(db_messages)
+        db_records = await service.find_all(conversationId=conv_id, limit=remaining)
+        messages.extend(
+            DBMessage.model_validate({**m.model_dump(), "_id": m.model_dump().pop("id")})
+            for m in db_records
+        )
 
     messages = sorted(messages, key=lambda m: m.id)
     return messages
