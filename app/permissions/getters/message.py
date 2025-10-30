1b65ff46fbe7a7f852bcc5859e985d0140764277
xifOO <wedyi28111@gmail.com>
2025-10-30 17:34:13 +0300

    feat: add cache message getter

--- app/permissions/getters/message.py
+++ app/permissions/getters/message.py
@@ -1,9 +1,18 @@
 from fastapi import Request
 
+from app.cache import RedisManager
 from app.services._service import BaseService
 
 
-async def get_message(request: Request, kwargs):
+async def get_db_message(request: Request, kwargs):
     service: BaseService = kwargs.get("service")
     message_id: str = kwargs.get("message_id")
     return await service.find_one(id=message_id)
+
+
+async def get_cache_message(request: Request, kwargs):
+    service: RedisManager = kwargs.get("service")
+    message_id: str = kwargs.get("message_id")
+    conv_id: str = kwargs.get("conv_id")
+    return await service.get_message(conv_id, message_id)
+
