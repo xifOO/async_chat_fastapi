126fefa0c4f4bda3e28b2a8f4b7a2647677c250c
xifOO <wedyi28111@gmail.com>
2025-10-30 17:34:07 +0300

    refactor: replace get_message with get_db_message and get_cache_message

--- app/permissions/getters/__init__.py
+++ app/permissions/getters/__init__.py
@@ -1,9 +1,10 @@
 from .conversation import get_conversation
-from .message import get_message
+from .message import get_db_message, get_cache_message
 from .users import get_user
 
 __all__ = [
     "get_conversation",
-    "get_message",
+    "get_db_message",
+    "get_cache_message",
     "get_user",
 ]
