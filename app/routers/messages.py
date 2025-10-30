b596ad1290d1602895f6c9257246754fc137bb26
xifOO <wedyi28111@gmail.com>
2025-10-30 17:34:21 +0300

    feat: add cache message update endpoint

--- app/routers/messages.py
+++ app/routers/messages.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request, Response
+from fastapi import APIRouter, Body, Depends, Query, Request, Response
 
+from app.cache import RedisManager
+from app.dependencies import get_redis_manager
 from app.permissions.decorators import check_own_or_permission, requires_check
-from app.permissions.getters import get_message
+from app.permissions.getters import get_db_message, get_cache_message
 from app.schemas.message import MessageUpdate
 from app.services.message import MessageService
 
@@ -20,7 +22,7 @@ MessageServiceDep = Annotated[MessageService, Depends(get_message_service)]
 @router.delete("/{message_id}", status_code=204)
 @requires_check(
     check_own_or_permission(
-        "message", "delete", get_object=get_message, owner_field="authorId"
+        "message", "delete", get_object=get_db_message, owner_field="authorId"
     )
 )
 async def delete_message(request: Request, service: MessageServiceDep, message_id: str):
@@ -31,11 +33,28 @@ async def delete_message(request: Request, service: MessageServiceDep, message_i
 @router.patch("/{message_id}")
 @requires_check(
     check_own_or_permission(
-        "message", "update", get_object=get_message, owner_field="authorId"
+        "message", "update", get_object=get_db_message, owner_field="authorId"
     )
 )
-async def update_message(
+async def update_db_message(
     request: Request, service: MessageServiceDep, message_id: str, data: MessageUpdate
 ):
     message = await service.update(pk=message_id, data=data)
+    return message
+
+
+@router.patch("/{message_id}/cache")
+@requires_check(
+    check_own_or_permission(
+        "message", "update", get_object=get_cache_message, owner_field="authorId"
+    )
+)
+async def update_cache_message(
+    request: Request, 
+    message_id: str, 
+    data: MessageUpdate = Body(...), 
+    conv_id: str = Query(...), 
+    service: RedisManager = Depends(get_redis_manager),
+):
+    message = await service.update_message(conv_id=conv_id, message_id=message_id, data=data)
     return message
