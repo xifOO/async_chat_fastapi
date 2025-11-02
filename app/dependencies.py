55f0b484d106c4059eb55619d36f711357e7c7b8
xifOO <wedyi28111@gmail.com>
2025-11-02 12:47:34 +0300

    feat: add AWS manager dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from app.aws import AWSManager
 from app.cache import RedisManager
 
 redis_manager = RedisManager()
@@ -11,4 +12,9 @@ def get_redis_manager() -> RedisManager:
     return redis_manager
 
 
+async def get_aws_manager() -> AWSManager:
+    return AWSManager()
+
+
 RedisManagerDep = Annotated[RedisManager, Depends(get_redis_manager)]
+AWSManagerDep = Annotated[AWSManager, Depends(get_aws_manager)]
